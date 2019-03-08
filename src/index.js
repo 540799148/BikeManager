617a9c8848cd9f44ef9c1ef3919a134ca0355f21
web-tutu <2505661782@qq.com>
2019-03-08 17:56:53 +0800

    添加路由

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Admin from './admin';
+import Router from './router'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <Admin />,
+    <Router />,
     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
